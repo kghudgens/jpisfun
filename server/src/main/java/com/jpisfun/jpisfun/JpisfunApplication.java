3f9038dfaadf95243c4274127b3e6c6137159f42
Kevin Hudgens <hudgens1073@gmail.com>
2023-05-06 16:44:52 -0500

    Created boilerplate project for java server

---
+++ server/src/main/java/com/jpisfun/jpisfun/JpisfunApplication.java
@@ -0,0 +1,13 @@
+package com.jpisfun.jpisfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpisfunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpisfunApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/jpisfun/jpisfun/JpisfunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpisfun.jpisfun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpisfunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
