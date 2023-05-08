0d765d47869cfa3b53929691525a789eb5b5e82f
Kevin Hudgens <hudgens1073@gmail.com>
2023-05-07 22:01:49 -0500

    Injected the resource dependency into the application class

--- resourceService/src/main/java/com/resourceService/resourceService/ResourceServiceApplication.java
+++ resourceService/src/main/java/com/resourceService/resourceService/ResourceServiceApplication.java
@@ -1,11 +1,20 @@
 package com.resourceService.resourceService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class ResourceServiceApplication {
 
+	@Autowired
+	private Resource resource;
+
+	public ResourceServiceApplication(Resource resource){
+		this.resource = resource;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ResourceServiceApplication.class, args);
 	}
